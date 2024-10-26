780ff3a907af19f65f1cb1c073d2e7d02db0ea1c
degleg <legoff.d@outlook.com>
2024-10-26 19:09:13 +0200

    Integrate hero section on homepage

---
+++ src/pages/homepage/Home.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "./components/herosection/Herosection"
+
+const Home = () => {
+    return (
+        <main>
+        <HeroSection/>
+        </main>
+    )
+}
+
+export default Home;
