c79ebfe60361f45eac69291ca8e98eababc3bb13
degleg <legoff.d@outlook.com>
2024-10-28 18:42:31 +0100

    Set up transaction cards

---
+++ src/pages/userpage/components/transactioncard/TransactionCard.jsx
@@ -0,0 +1,23 @@
+import Button from "../../../../common/components/button/Button";
+import "./transactioncard.css"
+
+const TransactionCard = ({ title, amount, description }) => {
+  return (
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{description}</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <Button
+          className="transaction-button"
+          type="button"
+          text="View transactions"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default TransactionCard;
