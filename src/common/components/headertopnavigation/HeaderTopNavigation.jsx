53842ace2b2998841ddc36719d3c56e0553a0f7b
degleg <legoff.d@outlook.com>
2024-10-29 15:36:18 +0100

    Add missing Navlinks to header (conditionals to add later)

--- src/common/components/headertopnavigation/HeaderTopNavigation.jsx
+++ src/common/components/headertopnavigation/HeaderTopNavigation.jsx
@@ -3,6 +3,7 @@ import "./headertopnavigation.css";
 import { NavLink } from "react-router-dom";
 
 const HeaderTopNavigation = () => {
+  //needs conditions to show specific ones based on state/token
   return (
     <nav className="main-nav">
       <NavLink className="main-nav-logo"  to="/">
@@ -10,6 +11,18 @@ const HeaderTopNavigation = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
       <div>
+      <NavLink className={({ isActive }) =>
+    `main-nav-item ${isActive ? "router-link-exact-active" : ""}`
+  } to="/user">
+          <i className="fa fa-user-circle"></i>
+          {' '}User{' '}
+        </NavLink>
+        <NavLink className={({ isActive }) =>
+    `main-nav-item ${isActive ? "router-link-exact-active" : ""}`
+  } to="/">
+          <i className="fa fa-sign-out"></i>
+          {' '}Sign Out{' '}
+        </NavLink>
         <NavLink className={({ isActive }) =>
     `main-nav-item ${isActive ? "router-link-exact-active" : ""}`
   } to="/sign-in">
