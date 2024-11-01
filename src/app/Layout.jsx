0c72611c33b6634ff277fe3f1c50cf3548deb4e1
degleg <legoff.d@outlook.com>
2024-11-01 21:48:50 +0100

    Add token and user info fetching (needs to be passed to children)

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -1,10 +1,62 @@
-import HeaderTopNavigation from '../common/components/headertopnavigation/HeaderTopNavigation';
-import Footer from '../common/components/footer/Footer';
+import HeaderTopNavigation from "../common/components/headertopnavigation/HeaderTopNavigation";
+import Footer from "../common/components/footer/Footer";
+import { useEffect, useState } from "react";
 
 const Layout = ({ children }) => {
+const [userToken, setUserToken] = useState(null)
+const [user, setUser] = useState(null)
+
+useEffect(() => {
+    const sessionToken = sessionStorage.getItem('token');
+  const localToken = localStorage.getItem('token');
+  const token = sessionToken || localToken;
+  if (token) {
+    setUserToken(token)
+    fetchUserInfo(token);
+  }
+
+}, [])
+
+
+  const fetchUserInfo = async (token) => {
+    try {
+      const response = await fetch(
+        "http://localhost:3001/api/v1/user/profile",
+        {
+          method: "GET",
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+          },
+        }
+      );
+      const data = await response.json();
+
+      //using data.status instead of response.status due to nesting in api response
+      if (data.status === 200) {
+        const { email, firstName, lastName, userName } = data.body;
+        setUser({ email, firstName, lastName, userName})
+       
+      } else {
+        switch (data.status) {
+          case 400:
+            console.log("Invalid Token");
+            break;
+          case 500:
+            console.log("Internal Server Error");
+            break;
+          default:
+            console.log("An error occurred");
+        }
+      }
+    } catch (error) {
+      console.log("The following error occurred: ", error);
+    }
+  };
+
   return (
-    <div className='body-layout'>
-      <HeaderTopNavigation />
+    <div className="body-layout">
+      <HeaderTopNavigation token={userToken} user={user} />
       {children}
       <Footer />
     </div>
