a961f3d9441d2de98b1a52baa004484c9e4f5a87
degleg <legoff.d@outlook.com>
2024-11-01 21:49:28 +0100

    Add token-related conditional rendering of navigation links

--- src/common/components/headertopnavigation/HeaderTopNavigation.jsx
+++ src/common/components/headertopnavigation/HeaderTopNavigation.jsx
@@ -2,33 +2,41 @@ import Logo from "../logo/Logo";
 import "./headertopnavigation.css";
 import { NavLink } from "react-router-dom";
 
-const HeaderTopNavigation = () => {
+const HeaderTopNavigation = ({ token, user }) => {
   //needs conditions to show specific ones based on state/token
+  //needs logout on link click
   return (
     <nav className="main-nav">
-      <NavLink className="main-nav-logo"  to="/">
+      <NavLink className="main-nav-logo" to="/">
         <Logo />
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
       <div>
-      <NavLink className={({ isActive }) =>
-    `main-nav-item ${isActive ? "router-link-exact-active" : ""}`
-  } to="/user">
-          <i className="fa fa-user-circle"></i>
-          {' '}User{' '}
-        </NavLink>
-        <NavLink className={({ isActive }) =>
-    `main-nav-item ${isActive ? "router-link-exact-active" : ""}`
-  } to="/">
-          <i className="fa fa-sign-out"></i>
-          {' '}Sign Out{' '}
-        </NavLink>
-        <NavLink className={({ isActive }) =>
-    `main-nav-item ${isActive ? "router-link-exact-active" : ""}`
-  } to="/sign-in">
-          <i className="fa fa-user-circle"></i>
-          {' '}Sign In{' '}
-        </NavLink>
+        {token && (
+          <NavLink
+            className={({ isActive }) =>
+              `main-nav-item ${isActive ? "router-link-exact-active" : ""}`
+            }
+            to="/user"
+          >
+            <i className="fa fa-user-circle"></i> User{" "}
+          </NavLink>
+        )}
+        {token && (
+          <NavLink className="main-nav-item" to="/">
+            <i className="fa fa-sign-out"></i> Sign Out{" "}
+          </NavLink>
+        )}
+        {!token && (
+          <NavLink
+            className={({ isActive }) =>
+              `main-nav-item ${isActive ? "router-link-exact-active" : ""}`
+            }
+            to="/sign-in"
+          >
+            <i className="fa fa-user-circle"></i> Sign In{" "}
+          </NavLink>
+        )}
       </div>
     </nav>
   );
