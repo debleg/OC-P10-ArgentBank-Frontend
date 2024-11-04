64c4252c3b1aedea92517818f912c932114d2bb7
degleg <legoff.d@outlook.com>
2024-11-04 11:59:05 +0100

    Update store with login reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "../features/Login/loginSlice";
 
 export default configureStore({
-    reducer: {},
+  reducer: { login: loginReducer },
 });
