25ec94c4652c24daee64fba50dd5dfe36305f14c
degleg <legoff.d@outlook.com>
2024-11-08 18:50:27 +0100

    Fix double API user call with useRef

--- src/features/User/User.jsx
+++ src/features/User/User.jsx
@@ -1,26 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userData } from "./userSlice";
 
 const User = () => {
   const dispatch = useDispatch();
-  const { firstName } = useSelector((state) => state.user);
-  const lastName = useSelector((state) => state.user.lastName);
+  const { firstName, lastName } = useSelector((state) => state.user);
   const token =
     sessionStorage.getItem("token") || localStorage.getItem("token");
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    const fetchUserInfo = async () => {
-      if (token) {
-        try {
-          await dispatch(userData({ token }));
-        } catch (error) {
-          console.log("User information retrieval failed", error);
+    // Without the following check React Strict Mode runs the function twice (double API call)
+    if (effectRan.current === false) {
+      const fetchUserInfo = async () => {
+        if (token) {
+          try {
+            await dispatch(userData({ token }));
+          } catch (error) {
+            console.log("User information retrieval failed", error);
+          }
         }
-      }
-    };
+      };
 
-    fetchUserInfo();
+      fetchUserInfo();
+      return () => {
+        effectRan.current = true;
+      };
+    }
   }, [token, dispatch]);
   return (
     <>
