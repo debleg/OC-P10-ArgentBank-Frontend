00bd00924146fdf772630a61b02c4231bc36e3fc
degleg <legoff.d@outlook.com>
2024-11-11 14:51:44 +0100

    Handle homepage refresh on logout to update information

--- src/features/Login/Logout.jsx
+++ src/features/Login/Logout.jsx
@@ -1,17 +1,24 @@
 import { useDispatch } from "react-redux";
 import { logout } from "./loginSlice";
 import { clearUserData } from "../User/userSlice";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 const Logout = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
+  const location = useLocation();
+  const dispatch = useDispatch();
 
   const handleLogout = (e) => {
     e.preventDefault();
-    dispatch(logout())
-    dispatch(clearUserData())
-    navigate("/");
+    dispatch(logout());
+    dispatch(clearUserData());
+    
+    //if the user is already on the home page, this allows the navbar to refresh to display the right information
+    if (location.pathname === "/") {
+      window.location.reload();
+    } else {
+      navigate("/");
+    }
   };
 
   return (
