8e4b7562ab2c336186d3a497ce13e5ae4dd2a2be
degleg <legoff.d@outlook.com>
2024-11-13 17:22:40 +0100

    Move logout process to utility file

--- src/features/Login/Logout.jsx
+++ src/features/Login/Logout.jsx
@@ -1,28 +1,16 @@
 import { useDispatch } from "react-redux";
-import { logout } from "./loginSlice";
-import { clearUserData } from "../User/userSlice";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { handleLogout } from "./logoutUtils";
 
 const Logout = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
 
-  const handleLogout = (e) => {
-    e.preventDefault();
-    dispatch(logout());
-    dispatch(clearUserData());
-    
-    //if the user is already on the home page, this allows the navbar to refresh to display the right information
-    if (location.pathname === "/") {
-      window.location.reload();
-    } else {
-      navigate("/");
-    }
-  };
+  const onLogout = handleLogout(navigate, location, dispatch)
 
   return (
-    <NavLink className="main-nav-item" onClick={handleLogout}>
+    <NavLink className="main-nav-item" onClick={onLogout}>
       <i className="fa fa-sign-out"></i> Sign Out{" "}
     </NavLink>
   );
---
+++ src/features/Login/logoutUtils.js
@@ -0,0 +1,14 @@
+import { logout } from "./loginSlice";
+import { clearUserData } from "../User/userSlice";
+
+export const handleLogout = (dispatch, navigate, location) => (e) => {
+  e.preventDefault();
+  dispatch(logout());
+  dispatch(clearUserData());
+  //if the user is already on the home page, this allows the navbar to refresh to display the right information
+  if (location.pathname === "/") {
+    window.location.reload();
+  } else {
+    navigate("/");
+  }
+};
